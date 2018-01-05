fa7aef3c44df0e5eecc993ae29743749dc900b6a
Marco <sancheztmarco@gmail.com>
2018-01-04 18:01:55 -0600

    carpetas

---
+++ app/src/main/java/com/example/planetmedia/serviceshealthkit/Network/RqObjets/AccessCodeRequest.kt
@@ -0,0 +1,8 @@
+package com.example.planetmedia.serviceshealthkit.Network.RqObjets
+
+/**
+ * Created by PLANETMEDIA on 04/01/2018.
+ */
+data class AcceessCodeRequest(
+        var temporal_token: String =""
+)
