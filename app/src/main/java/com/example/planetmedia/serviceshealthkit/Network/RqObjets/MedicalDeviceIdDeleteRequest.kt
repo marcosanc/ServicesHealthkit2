28f666704a3dc32a816188f18820f8d592d36ee1
Eder Gomez <eder.gomez@planetmedia.com.mx>
2018-01-08 14:18:33 -0600

    kpjo

--- app/src/main/java/com/example/planetmedia/serviceshealthkit/Network/RqObjets/MedicalDeviceIdDeleteRequest.kt
+++ app/src/main/java/com/example/planetmedia/serviceshealthkit/Network/RqObjets/MedicalDeviceIdDeleteRequest.kt
@@ -3,7 +3,7 @@ package com.victoria.planetmedia.androidtabvictoria.network.RqObjects
 /**
  * Created by ing.juanmejia on 08/01/18.
  */
-data class MedicalDeviceIdDelete (
+data class MedicalDeviceIdDeleteRequest(
         var access_token: String= "",
         var medicalDeviceId: String =""
 
