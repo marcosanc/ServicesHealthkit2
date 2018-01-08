673c8e58837a188ca5936e119b4c704c8c372ec2
Marco <sancheztmarco@gmail.com>
2018-01-08 11:51:14 -0600

    servicios

---
+++ app/src/main/java/com/example/planetmedia/serviceshealthkit/Network/RsObjets/ResponseCatalogGender.kt
@@ -0,0 +1,11 @@
+package com.example.planetmedia.serviceshealthkit.Network.RsObjets
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by PLANETMEDIA on 08/01/2018.
+ */
+data class ResponseCatalogGender (
+        @field:SerializedName("code")
+        val codigo:String?=null
+)
